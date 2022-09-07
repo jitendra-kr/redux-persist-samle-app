e950fc332c37f7069cc090ca5cd0a95e9ef41900
Jitendra Kumar <Jitendra.1.k@coforge.com>
2022-09-07 15:18:13 +0530

    used counter from slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAppDispatch, useAppSelector } from "./common/hooks";
+import { decrement, increment } from "./common/store/counterSlice";
+import type { RootState } from "./common";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state: RootState) => state.counter.value)
+
+
+  const inc = () => {
+    dispatch(increment());
+  };
+
+  const dec = () => {
+    dispatch(decrement());
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +26,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button>DEC</button> {count}
+          <button style={{marginLeft: "5px"}} onClick={inc} >INC</button>
+        </div>
       </header>
     </div>
   );
